ddef33c22e55191cced36528690f245ac82aaa98
ulsanether <hangmini12@naver.com>
2025-01-17 18:02:42 +0900

    초기 커밋.

---
+++ TaskTurner/Model/Task.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTurner.Model {
+   public class Task {
+      public int Id { get; set; }
+      public string Title { get; set; }
+      public string Description { get; set; }
+      public DateTime DueDate { get; set; }
+      public DateTime StartDate { get; set; }
+      public TimeSpan timer { get; set; }
+
+      public bool IsComplete { get; set; }
+
+      public TimeSpan Timer { get; set; }
+
+
+      public TaskStatus TaskStatus { get; set; }
+      public TaskImportance TaskImportance { get; set; }
+      public TaskCategory TaskCategory { get; set; }
+
+      }
+
+
+   public enum TaskStatus { 
+      InProgress,
+      Complete,
+      NotStarted,
+      Late,
+      Archived,
+      Deleted
+   
+   
+   }
+
+   public enum TaskCategory { 
+   
+      Work,
+      Personal,
+      Home,
+      HealthWelbeing,
+      Finance,
+      Shopping,
+      SocialFamily,
+      Education,
+      Travel,
+      Errands,
+      HobbiesLeisure,
+      VolunterringCommunity,
+      BirthdaysAnniversaries,
+      Projects,
+      LongTermGoals
+     
+   }
+   public enum TaskImportance { 
+   
+     Low,
+     Medium,
+     High,
+     Critical
+   
+   }
+   
+   
+   }
---
+++ TaskTurner/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTurner.ViewModel {
+   internal class MainWindowViewModel:INotifyPropertyChanged {
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      protected virtual void OnPropertyChanged(string propertyName) { 
+      
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      
+      }
+      }
+   }
